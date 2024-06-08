f9963ecba0a8a6ab6835f94b64c2b709a1f316c0
Tess <tess.mueske@gmail.com>
2024-06-08 17:27:01 +0200

    complete lab

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,17 @@
 from user import User
 
 class Student(User):
-    
-    def learn(self):
-        pass
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)     
+        self.knowledge = []   
+
+    def learn(self, string):
+        self.knowledge.append(string)
+
+
+# We've given you a barebones Student class. Change the class definition so that it inherits from the User class. Run the test suite and notice that you are passing some tests for the Student class, even without writing any code inside that class. That is because it will inherit the first_name and last_name attributes from the User class you told it to inherit from.
+
+# Individual students should initialize with an attribute, self.knowledge, that points to an empty list.
+
+# Expand the learn() method in the Student class so that in takes in a string and adds that string to the student's self.knowledge list.
+
--- lib/teacher.py
+++ lib/teacher.py
@@ -5,6 +5,22 @@ from user import User
 import random
 
 class Teacher(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)        
+        self.knowledge = [
+            "str is a data type in Python",
+            "programming is hard, but it's worth",
+            "JavaScript async web request",
+            "Python function call definition",
+            "object-oriented teacher instance",
+            "programming computers hacking learning terminal",
+            "pipenv install pipenv shell",
+            "pytest -x flag to fail fast",
+        ]
 
     def teach(self):
-        pass
+        random_index = random.randint(0, 8)
+        return self.knowledge[random_index]
+        
+
+# Expand the teach() method in the Teacher class so that it returns a random element from self.knowledge. We have imported Python's random library to assist you. You will want to use the random.randint() method to choose a random index in self.knowledge. This method takes two arguments, a minimum number and a maximum number, and returns a random element in the range.
